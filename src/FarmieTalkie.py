ec275639631af4fc54cbc7db01885edac6330d57
Asim-A <asimabazi@hotmail.no>
2021-04-20 08:06:16 +0200

    Refactored and corrected syntax mistakes 🔥

--- src/FarmieTalkie.py
+++ src/FarmieTalkie.py
@@ -1,4 +1,5 @@
 import stmpy
+import FarmieTalkieStates
 
 def generate_disconnect_transition(source: str) -> dict:
     disconnect_transition = {
@@ -35,97 +36,13 @@ def generate_playback_finished(source: str, target: str) -> dict:
 
 class FarmieTalkie:
     def __init__(self):
-		self.transitions = []
+		self.transitions = FarmieTalkieStates.get_transitions()
         
         # States done, check src/FarmieTalkieStates.py
 		self.states = FarmieTalkieStates.get_states()
 
 		# auto-generate similar transitions
-		states_with_disconnects = [
-			"initial", "main", "select_channel", "sending_message", "recording_message", 
-			"select_channel", "add_channel", "manage_channels"
-		]
-		for st in states_with_disconnects:
-			self.transitions.append(generate_disconnect_transition(st))
 		
-        states_that_allow_recording = ["main", "select_channel"]
-        for st in states_that_allow_recording:
-            self.transitions.append(generate_record_button_transition(st))
-
-		states_with_back_button_to_main = [
-			"play_message", "select_channel", "view_log", "manage_channels"
-		]
-		for st in states_with_back_button_to_main:
-			self.transitions.append(generate_back_button_transistion(st))
-
-		self.transitions.append(generate_back_button_transistion("replay_message", "view_log"))
-		self.transitions.append(generate_back_button_transistion("add_channel", "manage_channels"))
-
-        states_with_playback_finished = [
-            ("play_message", "main"), ("replay_message", "view_log")
-        ]
-        for st in states_with_playback_finished:
-            self.transitions.append(generate_playback_finished(source=st[0], target=st[1))
-
-		# Other, more specialized transitions
-		self.transitions.append({
-			"source": "disconnected",
-			"target": "main",
-			"trigger": "connected",
-			"effect": "subscribe_stored_channels"
-		})
-		self.transitions.append({
-			"source": "main",
-			"target": "select_channel",
-			"trigger": "select_button"
-		})
-		self.transitions.append({
-			"source": "recording_message",
-			"target": "sending_message",
-			"trigger": "record_button_release"
-		})
-		self.transitions.append({
-			"source": "sending_message",
-			"target": "main",
-			"trigger": "message_sent",
-			"effect": "play_confirmation_sound"
-		})
-		self.transitions.append({
-			"source": "main",
-			"target": "manage_channels",
-			"trigger": "manage_button"
-		})
-		self.transitions.append({
-			"source": "manage_channels",
-			"target": "add_channel",
-			"trigger": "add_button"
-		})
-		self.transitions.append({
-			"source": "add_channel",
-			"target": "manage_channels",
-			"trigger": "confirm_button",
-			"effect": "subscribe(channel)" # TODO: unsure of syntax
-		})
-		self.transitions.append({
-			"source": "main",
-			"target": "view_log",
-			"trigger": "view_log_button"
-		})
-		self.transitions.append({
-			"source": "view_log",
-			"target": "replay_message",
-			"trigger": "message_clicked"
-		})
-		self.transitions.append({
-			"source": "replay_message",
-			"target": "view_log",
-			"trigger": "playback_finished"
-		})
-		self.transitions.append({
-			"source": "replay_message",
-			"target": "view_log",
-			"trigger": "back_button"
-		})
 		
 
 
@@ -134,6 +51,6 @@ farmietalkie_machine = FarmieTalkie()
 stmpy.Machine(
 	"FarmieTalkie", 
 	farmietalkie_machine, 
-	farmietalkie_machine.transitions
+	farmietalkie_machine.transitions,
 	states=farmietalkie_machine.states
 )
