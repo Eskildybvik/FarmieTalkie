cb2bb80e6e531ecffd2c8ac3af0ef0d5cc3475fc
Asim-A <asimabazi@hotmail.no>
2021-04-20 08:06:58 +0200

    removed redundant functions in FarmieTalkie.py 🔥

--- src/FarmieTalkie.py
+++ src/FarmieTalkie.py
@@ -1,39 +1,6 @@
 import stmpy
 import FarmieTalkieStates
-
-def generate_disconnect_transition(source: str) -> dict:
-    disconnect_transition = {
-        "source": source,
-        "target": "disconnected",
-		"trigger": "disconnect"
-    }
-    return disconnect_transition
     
-def generate_record_button_transition(source: str) -> dict:
-    record_button_transition = {
-        "source": source,
-		"trigger": "record_button_press",
-        "target": "recording_message"
-    }
-    return record_button_transition
-
-def generate_back_button_transistion(source: str, target: str = "main") -> dict:
-	back_button_transition = {
-		"source": source,
-		"trigger": "back_button_press",
-		"target": target
-	}
-	return back_button_transition
-
-def generate_playback_finished(source: str, target: str) -> dict:
-    playback_finished_transition = {
-        "source": source,
-        "trigger": "playback_finished",
-        "target": target,
-    }
-    return playback_finished_transition
-    
-
 class FarmieTalkie:
     def __init__(self):
 		self.transitions = FarmieTalkieStates.get_transitions()
