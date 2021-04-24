d0f1bf2a752c278e973b1b7523dd464d29535f15
Eskil Dybvik <contact@esdy.io>
2021-04-24 13:16:37 +0200

    Make indentation consistent

--- src/MessageManager.py
+++ src/MessageManager.py
@@ -8,142 +8,142 @@ import math
 
 # Creates folder if it does not already exist
 def create_folder(folder_name: str):
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
+	if not os.path.exists(folder_name):
+		os.makedirs(folder_name)
 
 def get_file_names(path: str) -> List[str]:
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-    return onlyfiles
+	onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+	return onlyfiles
 
 class MessageManager:
-    """Class for helping keep track of the 10 messages in the log."""
-    
-    FOLDER_NAME = "messages"
-    PATH = f"./{FOLDER_NAME}"
-    MESSAGE_LIMIT = 10
-    FILE_NAME_PATTERN_STRING = "\d+\.ogg$"
-    FILE_NAME_PATTERN = re.compile(FILE_NAME_PATTERN_STRING)
-    FILE_NUMBER_PATTERN_STRING = "(\d+)\."
-    FILE_NUMBER_PATTERN = re.compile(FILE_NUMBER_PATTERN_STRING)
-     
-    def __init__(self):
-        self.__logger = logging.getLogger(__name__)
-        create_folder(self.FOLDER_NAME)
+	"""Class for helping keep track of the 10 messages in the log."""
+	
+	FOLDER_NAME = "messages"
+	PATH = f"./{FOLDER_NAME}"
+	MESSAGE_LIMIT = 10
+	FILE_NAME_PATTERN_STRING = "\d+\.ogg$"
+	FILE_NAME_PATTERN = re.compile(FILE_NAME_PATTERN_STRING)
+	FILE_NUMBER_PATTERN_STRING = "(\d+)\."
+	FILE_NUMBER_PATTERN = re.compile(FILE_NUMBER_PATTERN_STRING)
+	 
+	def __init__(self):
+		self.__logger = logging.getLogger(__name__)
+		create_folder(self.FOLDER_NAME)
 
-    def get_current_messages(self) -> List[bytearray]:
-        """ This method loads all the messages from the path into memory. 
-        
-        Path is standard './messages". This method should be called anew 
-        every time a new message is added so that the data is never stale. 
-        """
+	def get_current_messages(self) -> List[bytearray]:
+		""" This method loads all the messages from the path into memory. 
+		
+		Path is standard './messages". This method should be called anew 
+		every time a new message is added so that the data is never stale. 
+		"""
 
-        file_names = get_file_names(self.PATH)
-        clean_file_names = self.clean_file_names(file_names)
-        files = []
+		file_names = get_file_names(self.PATH)
+		clean_file_names = self.clean_file_names(file_names)
+		files = []
 
-        for fn in clean_file_names:
-            path = f"{self.PATH}/{fn}"
-            with open(path, "rb") as f:
-                file = f.read()
-                files.append(file)
+		for fn in clean_file_names:
+			path = f"{self.PATH}/{fn}"
+			with open(path, "rb") as f:
+				file = f.read()
+				files.append(file)
 
-        return files
-    
-    def get_current_message_names(self) -> List[str]:
-        """ This method loads the name of all saved messages.
-        
-        Path is standard './messages". This method should be called anew 
-        every time a new message is added so that the data is never stale. 
-        """
-        file_names = get_file_names(self.PATH)
-        clean_file_names = self.clean_file_names(file_names)
-        return [f"{self.PATH}/{n}" for n in clean_file_names]
+		return files
+	
+	def get_current_message_names(self) -> List[str]:
+		""" This method loads the name of all saved messages.
+		
+		Path is standard './messages". This method should be called anew 
+		every time a new message is added so that the data is never stale. 
+		"""
+		file_names = get_file_names(self.PATH)
+		clean_file_names = self.clean_file_names(file_names)
+		return [f"{self.PATH}/{n}" for n in clean_file_names]
 
-    def message_amount_within_limit(self, file_names: list) -> bool:
-        amount = len(file_names)
+	def message_amount_within_limit(self, file_names: list) -> bool:
+		amount = len(file_names)
 
-        if amount < self.MESSAGE_LIMIT:
-            return True
-        else:
-            return False
-    
-    def find_largest_number(self, clean_file_names: list) -> int:
-        largest = -math.inf
+		if amount < self.MESSAGE_LIMIT:
+			return True
+		else:
+			return False
+	
+	def find_largest_number(self, clean_file_names: list) -> int:
+		largest = -math.inf
 
-        for name in clean_file_names:
-            number = self.FILE_NUMBER_PATTERN.search(name)
-            group = number.group()
-            group = group[:-1]
-            possible_largest = int(group)
-            largest = max(largest, possible_largest)
-        
-        return largest
-
-
-    def find_smallest_number(self, clean_file_names:list) -> int:
-        smallest = math.inf
-
-        for name in clean_file_names:
-            number = self.FILE_NUMBER_PATTERN.search(name)
-            group = number.group()
-            group = group[:-1]
-            possible_smallest = int(group)
-            smallest = min(smallest, possible_smallest)
-        
-        return smallest
+		for name in clean_file_names:
+			number = self.FILE_NUMBER_PATTERN.search(name)
+			group = number.group()
+			group = group[:-1]
+			possible_largest = int(group)
+			largest = max(largest, possible_largest)
+		
+		return largest
 
 
-    def new_message_name(self, clean_file_names: list) -> str:
-        current_amount = len(clean_file_names)
+	def find_smallest_number(self, clean_file_names:list) -> int:
+		smallest = math.inf
 
-        if current_amount <= 0:
-            return f"{self.PATH}/0.ogg"
+		for name in clean_file_names:
+			number = self.FILE_NUMBER_PATTERN.search(name)
+			group = number.group()
+			group = group[:-1]
+			possible_smallest = int(group)
+			smallest = min(smallest, possible_smallest)
+		
+		return smallest
 
-        last_number = self.find_largest_number(clean_file_names)
-        new_last_item_number = last_number + 1
-        new_last_item_file_name = f"{self.PATH}/{new_last_item_number}.ogg"
-        
-        return new_last_item_file_name
 
-    def write_message(self, path: str, message: bytearray):
-        with open(path, "wb") as f:
-            f.write(message)
+	def new_message_name(self, clean_file_names: list) -> str:
+		current_amount = len(clean_file_names)
 
-    def clean_file_names(self, file_names):
-        names = [x for x in file_names if self.FILE_NAME_PATTERN.match(x)]
-        names.sort(key=(lambda x: int(self.FILE_NUMBER_PATTERN.findall(x)[0])))
-        return names
+		if current_amount <= 0:
+			return f"{self.PATH}/0.ogg"
 
-    def new_message(self, message: bytearray):
-        """ Saves message to persistent storage. If the new message 
-        increases the total count above the limit, then the message 
-        with lowest count (effectivly the oldest message) is deleted. 
-        The new message will be named like: "x.ogg" where x is a number. 
-        This number is found by finding the message with the largest 
-        number and then increment it by one. 
+		last_number = self.find_largest_number(clean_file_names)
+		new_last_item_number = last_number + 1
+		new_last_item_file_name = f"{self.PATH}/{new_last_item_number}.ogg"
+		
+		return new_last_item_file_name
 
-        Eg. if the file with filename "36.ogg" is the number which is 
-        largest, then the new message will be called "37.ogg". 
+	def write_message(self, path: str, message: bytearray):
+		with open(path, "wb") as f:
+			f.write(message)
 
-        Limit is 9223372036854775807. 
-        """
-        
-        file_names = get_file_names(self.PATH)
-        clean_file_names = self.clean_file_names(file_names)
-        is_within_limit = self.message_amount_within_limit(clean_file_names)
+	def clean_file_names(self, file_names):
+		names = [x for x in file_names if self.FILE_NAME_PATTERN.match(x)]
+		names.sort(key=(lambda x: int(self.FILE_NUMBER_PATTERN.findall(x)[0])))
+		return names
 
-        if is_within_limit:
-            new_last_item_file_name = self.new_message_name(clean_file_names)
-            #print(f"Is within limit and: {new_last_item_file_name}")
+	def new_message(self, message: bytearray):
+		""" Saves message to persistent storage. If the new message 
+		increases the total count above the limit, then the message 
+		with lowest count (effectivly the oldest message) is deleted. 
+		The new message will be named like: "x.ogg" where x is a number. 
+		This number is found by finding the message with the largest 
+		number and then increment it by one. 
 
-            self.write_message(new_last_item_file_name, message)
+		Eg. if the file with filename "36.ogg" is the number which is 
+		largest, then the new message will be called "37.ogg". 
 
-        else:
-            first_file_name = self.find_smallest_number(clean_file_names)
-            first_file_path = f"{self.PATH}/{first_file_name}.ogg"  
-            new_last_item_file_name = self.new_message_name(clean_file_names)
-            self.__logger.debug(f"Is not within limit and new: {new_last_item_file_name}, remove: {first_file_path}")
+		Limit is 9223372036854775807. 
+		"""
+		
+		file_names = get_file_names(self.PATH)
+		clean_file_names = self.clean_file_names(file_names)
+		is_within_limit = self.message_amount_within_limit(clean_file_names)
 
-            os.remove(first_file_path)
-            self.write_message(new_last_item_file_name, message)
-        
+		if is_within_limit:
+			new_last_item_file_name = self.new_message_name(clean_file_names)
+			#print(f"Is within limit and: {new_last_item_file_name}")
+
+			self.write_message(new_last_item_file_name, message)
+
+		else:
+			first_file_name = self.find_smallest_number(clean_file_names)
+			first_file_path = f"{self.PATH}/{first_file_name}.ogg"  
+			new_last_item_file_name = self.new_message_name(clean_file_names)
+			self.__logger.debug(f"Is not within limit and new: {new_last_item_file_name}, remove: {first_file_path}")
+
+			os.remove(first_file_path)
+			self.write_message(new_last_item_file_name, message)
+		
